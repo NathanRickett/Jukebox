a3234fa1f12007558f59132849ce0ad4f0fa8429
Nathan <nathanrickett@hotmail.com>
2024-11-16 20:56:36 +0000

    fixed python virtual environment

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jukebox/jukebox_app/env"
+set -gx VIRTUAL_ENV "/home/jukebox/Jukebox/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
